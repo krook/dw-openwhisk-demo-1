bc107c600cf03255c2e9cd88f1108b6286a54a46
Daniel Krook <krook@us.ibm.com>
2016-12-12 16:41:49 -0500

    Update deployment script.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
+#
+# Copyright 2016 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the “License”);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an “AS IS” BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# Capture the namespace where actions will be created
 WSK='wsk' # Set if not in your $PATH
 CURRENT_NAMESPACE=`$WSK property get --namespace | sed -n -e 's/^whisk namespace//p' | tr -d '\t '`
 echo "Current namespace is $CURRENT_NAMESPACE."
 
 function install() {
   $WSK action create handler handler.js
-  $WSK action invoke --blocking handler
 
   $WSK trigger create every-20-seconds \
     --feed  /whisk.system/alarms/alarm \
@@ -17,8 +31,6 @@ function install() {
     invoke-periodically \
     every-20-seconds \
     handler
-
-  $WSK activation poll
 }
 
 function uninstall() {
